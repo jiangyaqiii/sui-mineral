44ec7a1dba43aeeeb75d58b8534fc658138507fb
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-05-13 13:27:40 +0800

    Update mineral_direct_get.sh

--- mineral_direct_get.sh
+++ mineral_direct_get.sh
@@ -6,8 +6,8 @@ cd mineral
 wget https://github.com/ronanyeah/mineral-app/releases/download/v1/linux.zip
 unzip linux.zip
 chmod +x mineral-linux
-screen -S mineral
 read -p "请输入你的sui钱包密钥: " key_screte
 echo '确保钱包有sui作为gas'
 export WALLET=$key_screte
-./mineral-linux mine
+screen -dmS mineral bash -c './mineral-linux mine'
+
